ef7a9b99b2caaf47a400696084bc06d3f901317e
Hexagonal3 <hexagonal3@hexagon3.local>
2018-11-15 12:38:29 +0500

    Initial Commit

---
+++ MultipartFormData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MultipartFormData
+//
+//  Created by Hexagonal3 on 15/11/2018.
+//  Copyright © 2018 Hexagonal3. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
